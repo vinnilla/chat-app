18af6a1cb39a4d5f26bf311ee05822a204daa33d
Vincent <vincentckhuang@gmail.com>
2016-10-13 11:18:59 -0700

    setup chat navigation

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -8,6 +8,49 @@
 
 	function main($scope, database) {
 
+		$scope.brand = "chat-box";
+
+		$scope.messages = [
+			{user: 'Sally', status: 'Online'},
+			{user: 'Corbin', status: 'Away'},
+			{user: 'Billy', status: 'Offline'}
+		]
+
+		// on view load
+		$scope.$on('$viewContentLoaded', function() {
+			distributeWidth();
+			setStatusColor();
+		})
+
+		// on window resize
+		$(window).resize(function() {
+			distributeWidth();
+		})
+
+		function distributeWidth() {
+			// distribute width of screen between navigation and ui-view
+			var navWidth = parseInt($('#chat-nav').css('width'));
+			$('#angular-body').css('width', `${window.innerWidth-navWidth}px`);		
+		}
+
+		function setStatusColor() {
+			$scope.messages.forEach(function(message) {
+				var element = $(`#${message.user}-status`);
+				var color;
+				if (message.status === 'Online') {
+					color = 'rgb(117,255,117)';
+				}
+				else if (message.status === 'Away') {
+					color = 'rgb(255,227,117)';
+				}
+				else if (message.status === 'Offline') {
+					color = 'rgb(255,117,117)';
+				}
+				element.css('background', color);
+			})
+		}
+
+
 	} // end of main
 
 })();
