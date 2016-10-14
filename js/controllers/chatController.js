3bdec459b88c62ec7b6bab85b52de64e5bf7f244
Vincent <vincentckhuang@gmail.com>
2016-10-13 21:02:42 -0700

    auto scroll to bottom of chat history

--- js/controllers/chatController.js
+++ js/controllers/chatController.js
@@ -11,8 +11,15 @@
 		$scope.mainData = mainData;
 
 		$scope.$on('$viewContentLoaded', function() {
-			// $rootScope.$apply();
+			// timeout is necessary because scroll bar does not load immediately
+			setTimeout(function() {
+				// auto scroll to bottom
+				var height = $('#chat-body').height();
+				$('#chat-body').scrollTop(height);
+			},1000)
 		})
+
+
 
 		$scope.checkSelf = function(user) {
 			if (user === mainData.user.name) {
